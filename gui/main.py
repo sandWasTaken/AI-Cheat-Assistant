3adb159bac4d9449670ee817e066cd453ccc0c58
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 17:20:36 -0500

    Add GUI shell with Dear PyGui setup

---
+++ gui/main.py
@@ -0,0 +1,9 @@
+import dearpygui
+
+def main():
+    dpgu.create_view("window", "AI-Cheat-Assistant")
+    dpgu.text("<A Future-Ai Interface - Ready To Rock!")
+    dpgu.run()
+
+if __name__ == "__main__":
+    main()
